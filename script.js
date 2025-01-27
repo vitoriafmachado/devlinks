3dad6b336ef92990d787c350a052ccb6463d2400
Vitória Freitas machado <machadosvitoriaa@gmail.com>
2025-01-27 18:34:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  //const img =document.querySelector('#profile img')
+
+  //substituir a imagem
+  // if (html.classList.contains("ligth")) {
+  //se tiver ligth mode ,adicionar a imagem ligth
+  //img.setAttribute()
+  //}
+
+  // else {
+  //se tiver sem ligth mode ,manter a imagem normal
+  //img.setAttribute('src','./assets/myvih-removebg-preview.png')
+  // }
+}
